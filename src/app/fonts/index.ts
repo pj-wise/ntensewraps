d395f8c6faa262c9cb40f97f1491df38700c085a
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-05-11 21:57:08 +0800

    add fonts

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const clashDisplay = localFont({
+  src: [
+    {
+      path: "./clash-display/ClashDisplay-Extralight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./clash-display/ClashDisplay-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./clash-display/ClashDisplay-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./clash-display/ClashDisplay-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./clash-display/ClashDisplay-Semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./clash-display/ClashDisplay-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
