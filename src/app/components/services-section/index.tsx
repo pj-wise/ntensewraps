a56dce88c7967a78ac5136d2f2a763292d7643a9
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-05-24 23:55:59 +0800

    add services section

---
+++ src/app/components/services-section/index.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+export const ServicesSection = () => {
+  const services = [
+    {
+      title: "Full & Partial Commercial Wraps",
+      imgSrc: "/img/service/service-1.jpg",
+    },
+    {
+      title: "Cosmetic Paint Protection",
+      imgSrc: "/img/service/service-2.jpg",
+    },
+    { title: "Color Change Wraps", imgSrc: "/img/service/service-3.jpg" },
+    {
+      title: "Interior/Exterior Detailing",
+      imgSrc: "/img/service/service-4.jpg",
+    },
+    { title: "Paint Correction", imgSrc: "/img/service/service-5.jpg" },
+    {
+      title: "Taillight/Headlight Tinting",
+      imgSrc: "/img/service/service-6.jpg",
+    },
+  ];
+
+  return (
+    <div className="w-full h-full min-h-96 layoutPadding pt-12 pb-10">
+      <h3 className="text-3xl sm:text-5xl font-semibold text-center">
+        What services do we offer?
+      </h3>
+      <div className="grid pt-8 gap-4 m grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-screen-lg mx-auto max-sm:px-[10%] h-[800px] sm:h-[500px] lg:h-[400px]">
+        {services.map(({ title, imgSrc }, index) => {
+          return (
+            <ServiceItem
+              key={`service-${index}`}
+              isSeeMore={index === 0}
+              title={title}
+              imgSrc={imgSrc}
+            />
+          );
+        })}
+      </div>
+      <p className="text-center text-sm max-w-[739px] mx-auto px-8 pt-4">
+        <span className="text-primary">Ntense Car Wraps</span> specializes in
+        car wrapping, detailing, and ceramic coating services, and {"we're"}{" "}
+        accredited to{" "}
+        <span className="text-destructive">System X Ceramic Protection</span>.
+      </p>
+    </div>
+  );
+};
+
+export const ServiceItem = ({ isSeeMore, title, imgSrc }: ServiceItemProps) => {
+  return (
+    <div className="relative overflow-hidden text-base font-medium rounded-3xl ">
+      <div className="absolute line-clamp-1 z-20 bg-primary text-background opacity-90 left-0 right-0 p-1 sm:p-3 px-6 text-center">
+        {title}
+      </div>
+      {isSeeMore ? (
+        <div className="absolute z-10 bg-[hsla(0,0%,20%,0.8)] bottom-0 top-0 left-0 right-0 text-center flex items-center justify-center">
+          <p className="mt-8 font-normal">SEE MORE</p>
+        </div>
+      ) : null}
+      <Image src={imgSrc} fill className=" object-fill" alt="Service-image " />
+    </div>
+  );
+};
+
+export interface ServiceItemProps {
+  isSeeMore?: boolean;
+  title: string;
+  imgSrc: string;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import AboutSection from "./components/about-section";
 import { LandingSection } from "./components/landing-section";
 import { Navbar } from "./components/navbar";
+import { ServicesSection } from "./components/services-section";
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between mx-auto overflow-x-hidden">
       <Navbar />
-
       <LandingSection />
       <AboutSection />
-
-      {/* <Services />
-      <Gallery />  */}
+      <ServicesSection />
     </main>
   );
 }
